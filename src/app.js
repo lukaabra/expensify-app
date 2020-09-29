af9d2da1d890365a081f48241f922dd893b30f57
Luka Abramović <lukaabramovic2@gmail.com>
2020-09-29 11:59:17 -0400

    Connecting app.js with the separated redux file. Splitting up all the actions, reducers and selectors to their own folders and files.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppRouter from './routers/AppRouter';
-import MainApp from './components/MainApp';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
+import AppRouter from './routers/AppRouter';
 import './styles/styles.css';
+
+const store = configureStore();
+
+const expenseOne = store.dispatch(addExpense({
+    description: 'Rent',
+    amount: 100,
+    createdAt: 1000
+}));
+
+const expenseTwo = store.dispatch(addExpense({
+    description: 'Coffee',
+    amount: 10,
+    createdAt: -1000
+}));
+
+store.dispatch(setTextFilter('rent'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
